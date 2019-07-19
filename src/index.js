8b59a2f0cfb01e598db9142165a8d4b11ad2e972
chenjun <402074940@qq.com>
2019-07-19 17:22:34 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+console.log(`Looks like we are in ${process.env.NODE_ENV} mode!`);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
-import { createStore, applyMiddleware,compose} from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducers from './reducers/index'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const loggerMiddleware = createLogger()
-
+const loggerMiddleware = createLogger();
 const store = createStore(
-  reducers,
-  // applyMiddleware(thunk, loggerMiddleware)
-  composeEnhancers(applyMiddleware(thunk,loggerMiddleware))
-)
-export default store
+	reducers,
+	// applyMiddleware(thunk, loggerMiddleware)
+	process.env.NODE_ENV === 'development' ? composeEnhancers(applyMiddleware(thunk, loggerMiddleware)) : composeEnhancers(applyMiddleware(thunk))
+);
+export default store;
